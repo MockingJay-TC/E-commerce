b8d27ba3ab4d942519f3fd7765718ac6c9a37416
Victor-Aremu <77357316+Victor-Aremu@users.noreply.github.com>
2021-03-22 18:25:08 +0000

    started with javascript and jquery

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// $("h2").css("color", "red");
+// writing in javascript first then convert to jquery
+
+let increment = document.querySelector('#plu');
+let decrement = document.getElementById('#min');
+
+// $('#plu').css("color", "red");
